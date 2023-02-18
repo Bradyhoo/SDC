072c3192431cd1d45dbffbfd61cddd8aff107b39
Jacob Fink <jacobifink@gmail.com>
2023-02-18 04:39:51 +0100

    Uploading models for deliverables 2/17

---
+++ models/answersPhotos.js
@@ -0,0 +1,19 @@
+const db = require('../pgDB.js');
+const { Pool } = require('pg');
+const pool = new Pool();
+
+module.exports = {
+
+  listPhotos: (answer_id) => {
+    return pool.query(`SELECT * FROM answers_photos WHERE answer_id = ${answer_id}`)
+    .then(res => res.rows)
+    .catch(err => console.log('error getting photos in models: ', err));
+  },
+  addPhotos: (answer_id, urls) => {
+    return urls.map((url) => {pool.query(`INSERT INTO answers_photos (answer_id, url) VALUES (${answer_id}, ${url})`)})
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log('error getting photos in models: ', err));
+  }
+}
---
+++ models/questions.js
@@ -0,0 +1,33 @@
+const db = require('../pgDB.js');
+const { Pool } = require('pg');
+const pool = new Pool();
+
+module.exports = {
+
+  listQuestions: (product_id) => {
+    return pool.query(`SELECT * FROM questions WHERE product_id = ${product_id}`)
+    .then(res => {
+      // console.log(res.rows)
+      return res.rows;
+    })
+    .catch(err => console.log('error getting questions in models: ', err))
+  },
+  addQuestion: (product_id, question) => {
+    console.log(question.body);
+    return pool.query(`INSERT INTO questions (product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES (${product_id}, '${question.body}', ${Date.now()}, '${question.asker_name}', '${question.asker_email}', 'f', 0)`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log('error posting question in models: ', err))
+  },
+  markQuestionHelpful: (question_id) => {
+    return pool.query(`UPDATE questions SET helpful = helpful + 1 WHERE question_id = ${question_id}`)
+    .then(res => console.log(res))
+    .catch(err => console.log('error updating helpful for question in models: ', err))
+  },
+  reportQuestion: (question_id) => {
+    return pool.query(`UPDATE questions SET reported = 't' WHERE question_id = ${question_id}`)
+    .then(res => console.log(res))
+    .catch(err => console.log('error reporting question in models: ', err))
+  }
+}
